539a279362852cdbbd75b05c450fdee5f0987bcd
Rajat M Kavalur <35443061+1Rajat1@users.noreply.github.com>
2021-07-24 11:17:01 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -30,6 +30,11 @@ PRODUCT_PACKAGES += \
 # APEX
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/ld.config.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/swcodec/ld.config.txt
+   
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-legacymm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
 
 # Audio
 PRODUCT_PACKAGES += \

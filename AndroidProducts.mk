41c44d3d765280661713c24d2b5aaf6cd19d6c46
Rajat M Kavalur <35443061+1Rajat1@users.noreply.github.com>
2021-08-06 19:37:10 +0530

    s2: aosp adaption

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_s2.mk
+    $(LOCAL_DIR)/aosp_s2.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_s2-user \
-    xtended_s2-userdebug \
-    xtended_s2-eng
+    aosp_s2-user \
+    aosp_s2-userdebug \
+    aosp_s2-eng

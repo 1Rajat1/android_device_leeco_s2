7ea4357b07e996909b131607f0a5275d1cd40cc7
Rajat M Kavalur <35443061+1Rajat1@users.noreply.github.com>
2021-08-10 19:04:01 +0530

    s2: lineage implementation

--- lineage_s2.mk
+++ lineage_s2.mk
@@ -42,6 +42,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-leeco
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=le_s2_ww \
     PRODUCT_NAME=Le2_WW \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001-6578210-release-keys"
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200705.002 6506677 release-keys"
 
-BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys

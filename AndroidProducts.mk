bd492d783ff8c1336e14b4cb04073cb15da9b7f3
Rajat M Kavalur <35443061+1Rajat1@users.noreply.github.com>
2021-08-20 14:36:05 +0530

    s2: Komodo Adaption

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_s2.mk
+    $(LOCAL_DIR)/komodo_s2.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_s2-user \
-    xtended_s2-userdebug \
-    xtended_s2-eng
+    komodo_s2-user \
+    komodo_s2-userdebug \
+    komodo_s2-eng

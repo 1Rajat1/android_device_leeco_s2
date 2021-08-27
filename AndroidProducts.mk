f21c106f7788b3835bc6723665b87a31d515dfee
Rajat M Kavalur <35443061+1Rajat1@users.noreply.github.com>
2021-08-28 01:05:41 +0530

    s2: havoc implementation

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/komodo_s2.mk
+    $(LOCAL_DIR)/havoc_s2.mk
 
 COMMON_LUNCH_CHOICES := \
-    komodo_s2-user \
-    komodo_s2-userdebug \
-    komodo_s2-eng
+    havoc_s2-user \
+    havoc_s2-userdebug \
+    havoc_s2-eng
